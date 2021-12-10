776120dd0262aa5d199f203e8dd7b08454c2ab59
Shakib40 <shakibjilani@gmail.com>
2021-12-10 21:52:06 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,28 @@
 const mongoose = require('mongoose');
 
-// User Schema
-
 const userSchema = new mongoose.Schema(
-    {
-      first_name: { type: String, required: true },
-      last_name: { type: String, required: false },
-      gender: { type: String, required: false, default: "Male" },
-      date_of_birth: { type: String, required: true },
-    },
-    {
-      versionKey: false,
-    }
-  );
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: false },
+    profile_pic: [{ type: String, required: true }],
+  },
+  {
+    versionKey: false,
+  }
+);
+module.exports = mongoose.model("users", userSchema);
+
+
+// const userSchema = new mongoose.Schema(
+//     {
+//       first_name: { type: String, required: true },
+//       last_name: { type: String, required: false },
+//       gender: { type: String, required: false, default: "Male" },
+//       date_of_birth: { type: String, required: true },
+//     },
+//     {
+//       versionKey: false,
+//     }
+//   );
   
- module.exports = mongoose.model("users", userSchema);
+//  module.exports = mongoose.model("users", userSchema);
