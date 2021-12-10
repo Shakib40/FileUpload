4e9260b7f2cfd2c1f196b8baa3b32f497d7b93f2
Shakib40 <shakibjilani@gmail.com>
2021-12-10 21:53:43 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,18 +11,3 @@ const userSchema = new mongoose.Schema(
   }
 );
 module.exports = mongoose.model("users", userSchema);
-
-
-// const userSchema = new mongoose.Schema(
-//     {
-//       first_name: { type: String, required: true },
-//       last_name: { type: String, required: false },
-//       gender: { type: String, required: false, default: "Male" },
-//       date_of_birth: { type: String, required: true },
-//     },
-//     {
-//       versionKey: false,
-//     }
-//   );
-  
-//  module.exports = mongoose.model("users", userSchema);
